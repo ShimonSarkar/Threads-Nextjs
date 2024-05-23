09564e677f59929c30308e4a09c19e020cd0dd3b
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2024-05-22 21:49:58 -0400

    first

---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,13 @@
+const RightSideBar = () => {
+  return (
+    <>
+      <section className="custom-scrollbar rightsidebar">
+        <div className="flex flex-1 flex-col justify-start">
+          <h3>Suggested Communities</h3>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default RightSideBar;

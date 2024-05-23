aea3d93548c480e0564db605b023ae0ff8f1cf9c
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2024-05-22 22:34:43 -0400

    fifth

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-        serverComponentsExternalPackages: ["mongoose"],
-    },
-    typescript: {
-        ignoreDuringBuilds: true,
-    },
+    // experimental: {
+    //     serverActions: true,
+    //     serverComponentsExternalPackages: ["mongoose"],
+    // },
+    // typescript: {
+    //     ignoreDuringBuilds: true,
+    // },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.

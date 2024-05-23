bf33190d2f540b224fe90dc90529bb0a5ff74d4d
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2024-05-22 23:23:30 -0400

    d

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!.*\\..*|_next).*)',
-    '/',
-    '/(api|trpc)(.*)',
+
   ],
 };

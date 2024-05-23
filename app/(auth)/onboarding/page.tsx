499244264a1b33d0686e28ff8e3b8a878568b9db
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2024-05-22 22:01:12 -0400

    fourth

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,17 +1,16 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
-  const userInfo = {};
 
   if (!user) return null; // to avoid typescript warnings
 
-  // const userInfo = await fetchUser(user.id);
-  // if (userInfo?.onboarded) redirect("/");
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user.id,
